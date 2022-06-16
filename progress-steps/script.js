d8e3bd4ff55d54c816552d1bef4a340efd71f907
zengdakai <zengdakai@keytop.com.cn>
2022-06-16 11:13:12 +0800

    第二日练习

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -1,49 +1,46 @@
-const progress = document.getElementById('progress')
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const circles = document.querySelectorAll('.circle')
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
 
-let currentActive = 1
+let currentActive = 1;
 
 next.addEventListener('click', () => {
-    currentActive++
-
-    if(currentActive > circles.length) {
-        currentActive = circles.length
-    }
-
-    update()
-})
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
+});
 
 prev.addEventListener('click', () => {
-    currentActive--
-
-    if(currentActive < 1) {
-        currentActive = 1
-    }
-
-    update()
-})
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+});
 
 function update() {
-    circles.forEach((circle, idx) => {
-        if(idx < currentActive) {
-            circle.classList.add('active')
-        } else {
-            circle.classList.remove('active')
-        }
-    })
-
-    const actives = document.querySelectorAll('.active')
-
-    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
-
-    if(currentActive === 1) {
-        prev.disabled = true
-    } else if(currentActive === circles.length) {
-        next.disabled = true
+  circles.forEach((circle, index) => {
+    if (index < currentActive) {
+      circle.classList.add('active');
     } else {
-        prev.disabled = false
-        next.disabled = false
+      circle.classList.remove('active');
     }
+  });
+  //   原有写法
+  //   const actives = document.querySelectorAll('.active')
+
+  //   progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+  progress.style.width =
+    ((currentActive - 1) / (circles.length - 1)) * 100 + '%';
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 }
